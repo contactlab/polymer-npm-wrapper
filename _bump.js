69fde3a1ba2bd5f60b836092382fc7e18dfd6447
Salvatore Laisa <moebiusmania@gmail.com>
2017-04-27 17:44:48 +0200

    prebuilt files

---
+++ _bump.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+const version = require('./../node_modules/@polymer/polymer/package.json').version;
+const current = require('./../package.json');
+const updated = Object.assign({}, current, { version: version } );
+
+console.log('Current Polymer version:', version);
+console.log('Current package.json version:', current.version);
+
+fs.writeFile('./../package.json', updated, function(err) {
+    if(err) {
+        return console.log(err);
+    } else {
+        return console.log('Succesfully updated package.json version to +', updated.version);
+    }
+}); 
+
+
--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import './../@polymer/polymer/polymer.html';
-
-// Exports Polymer global object as module
-export const Polymer = window.Polymer;
