c5607d4227816698e68ef71ae9824aa9c701b436
Salvatore Laisa <moebiusmania@gmail.com>
2017-04-27 15:48:57 +0200

    rc4

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './node_modules/@polymer/polymer/polymer-element.html'
+  ],
+  output: {
+    path: path.join(__dirname, '.'),
+    filename: 'polymer-element.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.html$/,
+      loaders: ['wc-loader']
+    }]
+  },
+  resolve: {
+    mainFields: ['browserify', 'browser', 'module', 'main']
+  }
+}
