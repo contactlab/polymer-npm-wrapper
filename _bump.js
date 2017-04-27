8bc5bcb016317a8241fc4c7a6105235f8c6747ef
Salvatore Laisa <moebiusmania@gmail.com>
2017-04-27 17:45:04 +0200

    updated build

--- _bump.js
+++ _bump.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const fs = require('fs');
-const version = require('./../node_modules/@polymer/polymer/package.json').version;
-const current = require('./../package.json');
+const version = require('./node_modules/@polymer/polymer/package.json').version;
+const current = require('./package.json');
 const updated = Object.assign({}, current, { version: version } );
 
 console.log('Current Polymer version:', version);
 console.log('Current package.json version:', current.version);
 
-fs.writeFile('./../package.json', updated, function(err) {
+fs.writeFile('./package.json', updated, function(err) {
     if(err) {
         return console.log(err);
     } else {
